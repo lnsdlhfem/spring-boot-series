b19c65372dcdfc7671d96784ca093833fa4db6cc
lnsdlhfem <sp1018@foxmail.com>
2017-10-16 16:07:43 +0800

    mongodb

---
+++ spring-boot-mongodb/src/main/java/com/lnsdlhfem/mongodb/entity/Person.java
@@ -0,0 +1,44 @@
+package com.lnsdlhfem.mongodb.entity;
+
+public class Person {
+
+    private long id;
+
+    private String name;
+
+    private String age;
+
+    private String sex;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
